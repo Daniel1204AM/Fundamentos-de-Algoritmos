e61fa40cef930d4c31cfab6853d3295478be0e80
Daniel1204AM <5432omemxd@gmail.com>
2025-04-10 11:51:27 -0500

    Subiendo archivos

---
+++ Semana3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio1();
+            Console.ReadKey();
+        }
+
+        static void ejercicio1()
+        {
+            string nombre;
+
+            Console.WriteLine("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("\n" + nombre + ", bienvenido al curso de Fundamentos de Algoritmos");
+        }
+    }
+}
