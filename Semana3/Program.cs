f177ffa372ecabc3f16be3a1356bb55b4e1d25e1
Daniel1204AM <5432omemxd@gmail.com>
2025-04-10 12:16:19 -0500

    ejercicio4

--- Semana3/Program.cs
+++ Semana3/Program.cs
@@ -10,7 +10,7 @@ namespace Semana3
     {
         static void Main(string[] args)
         {
-            ejercicio2();
+            ejercicio4();
             Console.ReadKey();
         }
 
@@ -27,6 +27,24 @@ namespace Semana3
         static void ejercicio2()
         {
             Console.WriteLine("Daniel\n22\nIng. Sistemas");
+        }
+
+        static void ejercicio4()
+        {
+            int num1, num2;
+
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("La suma es: " + (num1 + num2));
+            Console.WriteLine("La resta es: " + (num1 - num2));
+            Console.WriteLine("La multiplicación es: " + (num1 * num2));
+            Console.WriteLine("La división es: " + (num1 / num2));
+            Console.WriteLine("El residuo es: " + (num1 % num2));
         }
     }
 }
