0bef01c0fc0127bb521c59309242569913d5afa4
rip van kinkle <vankinkle@gmail.com>
2025-05-15 12:18:03 -0500

    s8

---
+++ Semana8.5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana8._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            char continuar;
+
+            do {
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicacion");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("0. Salir");
+
+                Console.WriteLine("Ingrese una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+            } while (opcion <0 || opcion >=5);
+        }
+
+        static void suma (int x, int y)
+        {
+
+        }
+
+        static void resta(int x, int y)
+        {
+
+        }
+
+        static void multi(int x, int y)
+        {
+
+        }
+
+        static void divi(int x, int y)
+        {
+
+        }
+    }
+}
