238c7289ff850546fc91dcf57f90c7506a20f020
rip van kinkle <vankinkle@gmail.com>
2025-05-22 12:16:23 -0500

    s9

---
+++ Biblioteca/Cuadrado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Cuadrado
+    {
+        public void area(int l)
+        {
+            Console.Write("\nÀrea: " + (l * l));
+        }
+
+        public void perimetro(int l)
+        {
+            Console.Write("\nPerimetro: " + (l + l + l + l));
+        }
+    }
+}
