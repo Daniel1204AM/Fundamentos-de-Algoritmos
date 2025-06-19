7768808e812b5873734feeca5da5b89107a9fe70
aran <bryan-alexander12@hotmail.com>
2025-06-19 11:46:23 -0500

    s13

---
+++ Semana13/Estudiantes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana13
+{
+    class Estudiantes
+    {
+        private string[] nombres = new string[0];
+        private byte[] edad = new byte[0];
+        private int cantidad = 0;
+
+        public void insertar() {
+            string continuar;
+            do {
+                Console.Clear();
+                Console.WriteLine("=== Registrar estudiantes ===\n");
+                Array.Resize(ref nombres, nombres.Length + 1);
+                Array.Resize(ref edad, edad.Length + 1);
+
+                Console.WriteLine("Ingrese sus nombres");
+                nombres[cantidad] = Console.ReadLine();
+                Console.Write("Ingrese su edad: ");
+                edad[cantidad] = byte.Parse(Console.ReadLine());
+                cantidad++;
+
+                Console.WriteLine("Se registro con exito! Desea seguir registrando (S): ?");
+                continuar = Console.ReadLine().ToLower();
+            } while (continuar == "s");
+        }
+
+        public void mostrar()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Estudiantes registrados ===\n");
+            if (cantidad != 0)
+            {
+                for (int i = 0; i < nombres.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {nombres[i]} {edad[i]}");
+                }
+            }
+            else
+                Console.WriteLine("\nNo hay registros de estudiantes!");
+            
+        }
+
+        public void buscar()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Buscar estudiante ===\n");
+            Console.Write("Ingrese el nombre del estudiante: ");
+            string nombreBuscar = Console.ReadLine();
+            bool encontrado = false;
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                if (nombres[i].ToLower() == nombreBuscar.ToLower())
+                {
+                    Console.WriteLine($"Estudiante encontrado: {nombres[i]}, Edad: {edad[i]}");
+                    encontrado = true;
+                    break;
+                }
+            }
+
+            if (!encontrado)
+                Console.WriteLine("Estudiante no encontrado.");
+        }
+    }
+}
---
+++ Semana13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            Estudiantes e = new Estudiantes();
+            do
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("=== MENÚ DE GESTIÓN DE ESTUDIANTES ===");
+                    Console.WriteLine("1. Registrar");
+                    Console.WriteLine("2. Mostrar");
+                    Console.WriteLine("3. Buscar");
+                    Console.WriteLine("4. Modificar");
+                    Console.WriteLine("5. Eliminar");
+                    Console.WriteLine("6. Ordenar");
+                    Console.WriteLine("7. Salir\n");
+                    Console.Write("Seleccione una opción: ");
+                    string entrada = Console.ReadLine();
+                    if (!int.TryParse(entrada, out opcion) || opcion < 1 || opcion > 7)
+                    {
+                        Console.Write("\nEntrada inválida. Presione una tecla para intentarlo nuevamente...");
+                        Console.ReadKey();
+                    }
+                } while (opcion < 1 || opcion > 7);
+                switch (opcion)
+                {
+                    case 1: e.insertar(); break;
+                    case 2: e.mostrar(); break;
+                    case 3: e.buscar(); break;
+                    case 4: break;
+                    case 5: break;
+                    case 6: break;
+                    case 7: Console.WriteLine("\nSaliendo del programa..."); return;
+                }
+                if (opcion != 7)
+                {
+                    Console.Write("\nPresione una tecla para regresar al menú...");
+                    Console.ReadKey();
+                }
+            } while (opcion != 7);
+        }
+    }
+}
